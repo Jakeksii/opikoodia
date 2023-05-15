655bf24d360e5b589c8a8e59a6188b6193b2b5dd
Jakeksii <jaakko.ruhanen@outlook.com>
2023-05-15 22:56:54 +0300

    Game of life

--- javascript/90_exercises/01_game-of-life/public/script.js
+++ javascript/90_exercises/01_game-of-life/public/script.js
@@ -1,45 +1,103 @@
 var canvas;
-var ctx;
-var running = 0;
+var context;
 var interval;
+var simulating = 0;
+
+var size = 500;
+var columns = 10;
+var rows = 10;
+var cellSize = 50;
+
+var board = [];
+
+
+class cell {
+    constructor (row, col, alive=false){
+        this.row = row;
+        this.col = col;
+        this.alive = alive;
+    }
+    draw(){
+        context.fillStyle = this.alive ? "black" : "white";
+        context.fillRect(this.row*cellSize, this.col*cellSize, cellSize, cellSize);
+    }
+}
+
+
 
 window.onload = function () {
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+    canvas.width = size;
+    canvas.height = size;
+    createBoard();
 }
 
-function createRect(){
-    let x,y = 0;
-    let side = 0;
-    let color = "#";
-    let colorpicker = "ABCDEF0123456789";
 
-    side = Math.floor((Math.random()*80)+20);
-    x = Math.floor((Math.random()*400)+1);
-    y = Math.floor((Math.random()*400)+1);
-    for (let i = 0; i<6;i++){
-        let temp = Math.floor(Math.random()*16);
-        color = color + colorpicker[temp];
+
+function createBoard() {
+    for(let col=0; col < columns; col++){
+        for(let row=0; row < rows; row++){
+            let alive = false;
+            /*if(col % 2 == 0){
+                alive = row % 2 == 0 ? true : false;
+            }else{
+                alive = row % 2 == 0 ? false : true;
+            }*/
+            
+            let newCell = new cell(row, col, alive);
+            board.push(newCell);
+        }
     }
+    console.log(board);
 
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, side, side);
+    board.forEach(cell => {
+        cell.draw();
+    });
 }
 
-function startCanvas(){
+function clickCell(event) {
+    var x = event.clientX - 10;
+    var y = event.clientY - 10;
 
-    if (running){
-        running = 0;
+    let cellRow = Math.floor(x / cellSize);
+    let cellCol = Math.floor(y / cellSize);
+    console.log(cellRow + " : " + cellCol);
+
+    let cell = board[cellCol*rows + cellRow] //board.find(cell => cell.row === cellRow && cell.col === cellCol);
+    cell.alive = !cell.alive;
+    cell.draw();
+}
+
+function startSimulation(){
+    simulate()
+    /*if(simulating){
+        simulating = 0;
         clearInterval(interval);
-        document.getElementById("startbtn").innerHTML = "Start";
     }else{
-        running = 1;
-        interval = setInterval(createRect, 200);
-        document.getElementById("startbtn").innerHTML = "Stop";
-    }
-    
+        simulating = 1;
+        interval = setInterval(simulate, 1000);
+    }*/
 }
 
-function clearCanvas(){
-    ctx.clearRect(0, 0, 500, 500);
+function simulate() { //needs to be faster  + ottaa jostain syystä vastakkaisen kulma naapuriksi
+    //let aliveCells = board.filter(cell => cell.alive === true);
+    board.forEach(cell => {
+        let neighboursAlive = 0;
+
+        const neighbours = [-1, -1, 0, -1, 1, -1, -1, 0, 1, 0, -1, 1, 0, 1, 1, 1];
+        let cellRow = cell.row;
+        let cellCol = cell.col;
+
+        console.log("Solu: " + cellRow + " : " + cellCol);
+
+        for(i=0; i<16; i+=2){
+            let neighbour = board[(cellCol+neighbours[i])*rows + (cellRow+neighbours[i+1])] //.find(cell => cell.row === (cellRow + neighbours[i]) && cell.col === (cellCol + neighbours[i+1]));
+            if(neighbour !== undefined){
+                console.log("naapuri "+ neighbour.row +" : " + neighbour.col + " " + neighbour.alive)
+                neighboursAlive += neighbour.alive ? 1 : 0;
+            }
+        }
+        console.log("naapurit hengissä "+ neighboursAlive + "\n")
+    });
 }
