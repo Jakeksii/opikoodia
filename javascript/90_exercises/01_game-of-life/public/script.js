acd3bcedc4b7ff2568737467124fef609cb18c64
Jakeksii <jaakko.ruhanen@outlook.com>
2023-05-16 09:11:11 +0300

    Game of life

--- javascript/90_exercises/01_game-of-life/public/script.js
+++ javascript/90_exercises/01_game-of-life/public/script.js
@@ -3,12 +3,12 @@ var context;
 var interval;
 var simulating = 0;
 
-var size = 500;
-var columns = 10;
-var rows = 10;
-var cellSize = 50;
+var SIZE = 500;
+var COLUMNS = 10;
+var ROWS = 10;
+var CELLSIZE = 50;
 
-var board = [];
+var BOARD = [];
 
 
 class cell {
@@ -16,10 +16,52 @@ class cell {
         this.row = row;
         this.col = col;
         this.alive = alive;
+        this.stateChange = false;
+        this.neighboursAlive = 0;
+
+        switch (true){
+            case (row === 0 && col === 0): //top left
+                this.neighbours = [1,0, 0,1, 1,1];
+                break;
+            case (row === 0 && col === COLUMNS-1): //top right
+                this.neighbours = [0,-1, 1,0, 1,-1];
+                break;
+            case (row === ROWS-1 && col === 0): //bottom right
+                this.neighbours = [-1,0, 0,-1, -1,-1];
+                break;
+            case (row === ROWS-1 && col === COLUMNS-1): //bottom left
+                this.neighbours = [-1,0, 0,1, -1,1];
+                break;
+            case (col === 0): //top
+                this.neighbours = [0,-1, 0,1, 1,-1, 1,0, 1,1];
+                break;
+            case (row === 0): //left
+                this.neighbours = [-1,0, -1,1, 0,1, 1,1, 1,0];
+                break;
+            case (row === ROWS-1): //right
+                this.neighbours = [-1,0, -1,-1, 0,-1, 1,-1, 1,0];
+                break;
+            case (col === COLUMNS-1): //bottom
+                this.neighbours = [0,-1, -1,-1, -1,0 -1,1, 0,1];
+                break;
+            default: //center
+                this.neighbours = [-1,-1, 0,-1, 1,-1, -1,0, 1,0, -1,1, 0,1, 1,1];
+                break;
+        }
     }
     draw(){
         context.fillStyle = this.alive ? "black" : "white";
-        context.fillRect(this.row*cellSize, this.col*cellSize, cellSize, cellSize);
+        context.fillRect(this.row*CELLSIZE, this.col*CELLSIZE, CELLSIZE, CELLSIZE);
+    }
+    checkNeighbours(){
+        this.neighboursAlive = 0;
+
+        for(let i=0; i<this.neighbours.length; i+=2){
+            let neighbour = BOARD[(this.col+this.neighbours[i])*ROWS + (this.row+this.neighbours[i+1])] //.find(cell => cell.row === (cellRow + neighbours[i]) && cell.col === (cellCol + neighbours[i+1]));
+            console.log("naapuri "+ neighbour.row +" : " + neighbour.col + " " + neighbour.alive)
+            this.neighboursAlive += neighbour.alive ? 1 : 0;
+        }
+        console.log("Solu: " + this.row + " : " + this.col + " naapurit hengissä "+ this.neighboursAlive + "\n")
     }
 }
 
@@ -28,30 +70,31 @@ class cell {
 window.onload = function () {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
-    canvas.width = size;
-    canvas.height = size;
+    canvas.width = SIZE;
+    canvas.height = SIZE;
     createBoard();
 }
 
 
 
 function createBoard() {
-    for(let col=0; col < columns; col++){
-        for(let row=0; row < rows; row++){
+    for(let col=0; col < COLUMNS; col++){
+        for(let row=0; row < ROWS; row++){
             let alive = false;
             /*if(col % 2 == 0){
-                alive = row % 2 == 0 ? true : false;
+                alive = row % 2 == 0 ? true : false; //Chess BOARD
             }else{
                 alive = row % 2 == 0 ? false : true;
             }*/
             
             let newCell = new cell(row, col, alive);
-            board.push(newCell);
+            BOARD.push(newCell);
         }
     }
-    console.log(board);
+    console.log(BOARD);
 
-    board.forEach(cell => {
+    //No need for draw if all starting cells are dead
+    BOARD.forEach(cell => {
         cell.draw();
     });
 }
@@ -60,11 +103,11 @@ function clickCell(event) {
     var x = event.clientX - 10;
     var y = event.clientY - 10;
 
-    let cellRow = Math.floor(x / cellSize);
-    let cellCol = Math.floor(y / cellSize);
+    let cellRow = Math.floor(x / CELLSIZE);
+    let cellCol = Math.floor(y / CELLSIZE);
     console.log(cellRow + " : " + cellCol);
 
-    let cell = board[cellCol*rows + cellRow] //board.find(cell => cell.row === cellRow && cell.col === cellCol);
+    let cell = BOARD[cellCol*ROWS + cellRow] //BOARD.find(cell => cell.row === cellRow && cell.col === cellCol);
     cell.alive = !cell.alive;
     cell.draw();
 }
@@ -81,23 +124,8 @@ function startSimulation(){
 }
 
 function simulate() { //needs to be faster  + ottaa jostain syystä vastakkaisen kulma naapuriksi
-    //let aliveCells = board.filter(cell => cell.alive === true);
-    board.forEach(cell => {
-        let neighboursAlive = 0;
-
-        const neighbours = [-1, -1, 0, -1, 1, -1, -1, 0, 1, 0, -1, 1, 0, 1, 1, 1];
-        let cellRow = cell.row;
-        let cellCol = cell.col;
-
-        console.log("Solu: " + cellRow + " : " + cellCol);
-
-        for(i=0; i<16; i+=2){
-            let neighbour = board[(cellCol+neighbours[i])*rows + (cellRow+neighbours[i+1])] //.find(cell => cell.row === (cellRow + neighbours[i]) && cell.col === (cellCol + neighbours[i+1]));
-            if(neighbour !== undefined){
-                console.log("naapuri "+ neighbour.row +" : " + neighbour.col + " " + neighbour.alive)
-                neighboursAlive += neighbour.alive ? 1 : 0;
-            }
-        }
-        console.log("naapurit hengissä "+ neighboursAlive + "\n")
+    //let aliveCells = BOARD.filter(cell => cell.alive === true);
+    BOARD.forEach(cell => {
+        cell.checkNeighbours();
     });
 }
